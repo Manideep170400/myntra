294dcc4f21f5d64461969e3ae8a2bc5f1761b76d
Manideep170400 <116505823+Manideep170400@users.noreply.github.com>
2024-07-16 15:47:30 +0530

    css cahnges

--- src/Main/homePage.js
+++ src/Main/homePage.js
@@ -3,27 +3,62 @@ import Oppo from "../Mobile/oppo";
 import { Link } from "react-router-dom";
 import Vivo from "../Mobile/vivo";
 import Iq from "../Mobile/iq";
+import "./HomePage.css";
 
 function HomePage() {
-  const [mobileOppo] = useState(<Oppo />);
+  const [mobile, setMobile] = useState(<Oppo />);
+
+  const handleMobile = (component) => {
+    setMobile(component);
+  };
 
   const object = {
     oppo: <Oppo />,
     vivo: <Vivo />,
     iq: <Iq />,
   };
+
   return (
-    <div className="header">
-      <div>
-        <Link to="/home/oppo" onClick={mobileOppo}>
-          oppo
-        </Link>
-        <Link to="/home/vivo">Vivo</Link>
-        <Link to="/home/iq">IQ</Link>
+    <div className="homepage">
+      <div className="sidebar">
+        <div className="header">Navigation</div>
+        <ul>
+          <li>
+            <Link
+              to="/home/oppo"
+              onClick={(e) => {
+                e.preventDefault();
+                handleMobile(object.oppo);
+              }}
+            >
+              Oppo
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/home/vivo"
+              onClick={(e) => {
+                e.preventDefault();
+                handleMobile(object.vivo);
+              }}
+            >
+              Vivo
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/home/iq"
+              onClick={(e) => {
+                e.preventDefault();
+                handleMobile(object.iq);
+              }}
+            >
+              IQ
+            </Link>
+          </li>
+        </ul>
       </div>
-      <div>{object.oppo}</div>
-      {object.vivo}
-      {object.iq}
+      <div className="content">{mobile}</div>
     </div>
   );
 }
