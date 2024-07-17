fe913bd7111840834b52288c7d9b4054d6f8f221
Manideep170400 <116505823+Manideep170400@users.noreply.github.com>
2024-07-17 11:37:07 +0530

    changes

--- src/pageLayout/contentPage.js
+++ src/pageLayout/contentPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import HomePage from "../Main/homePage";
 import { Link } from "react-router-dom";
 import HeroCycle from "../Cycle/altas";
@@ -6,15 +6,29 @@ import HeroCycle from "../Cycle/altas";
 function ContentPage() {
   const { info } = useState(<HomePage />);
   const { cycle } = useState(<HeroCycle />);
+  const [show, setShow] = useState(false);
+
   return (
     <div>
       <p>Content Page</p>
-      <Link to="/home" onClick={info}>
-        Mobile
-      </Link>
-      <Link to="/cycle" onClick={cycle}>
-        BiCycle
-      </Link>
+      <span
+        class="material-symbols-outlined"
+        onClick={() => {
+          setShow(!show);
+        }}
+      >
+        arrow_drop_down
+        {show && (
+          <div>
+            <Link to="/home" onClick={info}>
+              Mobile
+            </Link>
+            <Link to="/cycle" onClick={cycle}>
+              BiCycle
+            </Link>
+          </div>
+        )}
+      </span>
     </div>
   );
 }
